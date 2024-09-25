706ffd3bf316e123a037280c79b94b44a7efdd64
Eli Manzo <96manzo.eli@gmail.com>
2024-09-25 15:26:37 -0700

    [Feat]: Added Week Card + Randomly Displayed Icebreakers (#42)
    * refactor(header): Removed some divs and structured the header to be the parent container for the header
    
    * wip(index.html): Added initial html + css to the week card component
    
    * feat(index.html): Added Font Awesome icons
    
    * feat(week card): Added collapse option for the week card
    
    * feat(random icebreakers): Implemented randomly displayed questions
    
    * feat(week card): Adjusting some mobile styles
    
    * chore(footer): Fixing footer to always bet at the bottom

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log("Hello, World!");
+import getQuestions from "./scripts/getQuestions.js";
+import shuffle from "./util/shuffle.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleIcon = document.getElementById("toggle-icon");
+  const weekCardContent = document.getElementById("week-card-content");
+
+  toggleIcon.addEventListener("click", () => {
+    // Toggle the collapsed class to control max-height
+    weekCardContent.classList.toggle("collapsed");
+
+    // Toggle the chevron icon rotation
+    toggleIcon.classList.toggle("rotate");
+  });
+
+  async function displayQuestions(amountOfQuestions = 2) {
+    let questions = await getQuestions(); // fetch questions
+    shuffle(questions); // shuffle them
+    questions = questions.slice(0, Math.max(0, amountOfQuestions)); // get the amountOfQuestions
+    const ol = document.getElementById("ice-breaker-questions");
+    ol.innerHTML = ""; // Clear any existing questions
+    for (const question of questions) {
+      const li = document.createElement("li");
+      li.textContent = question;
+      ol.appendChild(li);
+    }
+  }
+
+  displayQuestions();
+});
