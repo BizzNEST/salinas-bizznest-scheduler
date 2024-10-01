1064c168802610b83f23a9a4c6f9d8facfbcdd0d
Jacky Perez <77761078+jacky-p@users.noreply.github.com>
2024-10-01 16:35:21 -0700

    [Feat]: Displaying Intern Pairings in Week card (#43)
    * feat(table): implemented intern display w/ dummy data
    
    * style(table): created a function to reduce repeated code
    
    * feat(table): added pairing fuction feeded into display
    
    * style(table): changed data structure from object to array
    
    * style(table): moved some functions and added odd length compatibilty
    
    * chore(table): changed the naming of department
    
    * chore(table): resolving requests
    
    * chore(table): removed temporary intern data
    
    * chore(table): removed commented out code

---
+++ src/scripts/getInterns.js
@@ -0,0 +1,13 @@
+export default async function getInterns() {
+  try {
+    const response = await fetch("src/documents/temp_interns.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data.interns;
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+    throw error;
+  }
+}
