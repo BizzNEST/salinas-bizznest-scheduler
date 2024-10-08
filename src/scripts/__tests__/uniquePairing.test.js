9f6a9a97efe3f0ea86fc44ab6d0d212470902676
Jacky Perez <jackyyp2402@gmail.com>
2024-10-08 14:46:56 -0700

    test(pairing): found/fixed bug with intern data, tried to implement .reduce

--- src/scripts/__tests__/uniquePairing.test.js
+++ src/scripts/__tests__/uniquePairing.test.js
@@ -1,6 +1,7 @@
 import shuffle from "../../util/shuffle.js";
 import { isValidPair, uniquePairingHelper } from "../filters.js";
 import pair from "../../util/pair.js";
+import accuracy from "../../util/accuracy.js";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
@@ -11,10 +12,10 @@ const __dirname = dirname(__filename);
 const internsPath = join(__dirname, "../../documents/interns.json");
 const internInfo = JSON.parse(readFileSync(internsPath, "utf-8"));
 
-async function pairInterns(isUniqueDept, isUniqueLoc) {
-  let interns = [];
+function pairInterns(isUniqueDept, isUniqueLoc) {
+  const interns = [];
   const fetchedInterns = internInfo;
-  for (const [intern, internInfo] of Object.entries(fetchedInterns)) {
+  for (const [intern, internInfo] of Object.entries(fetchedInterns.interns)) {
     interns.push({
       name: intern,
       ...internInfo,
@@ -26,11 +27,18 @@ async function pairInterns(isUniqueDept, isUniqueLoc) {
   return pair(interns);
 }
 
-async function uniquePairingTest(test) {
+function uniquePairingTest(test) {
   let count = 0;
-  const pairs = await pairInterns(test.isUniqueDept, test.isUniqueLoc);
+  const pairs = pairInterns(test.isUniqueDept, test.isUniqueLoc);
 
-  pairs.forEach((pair) => {
+  count = pairs.reduce((accumultor, currentValue) => {
+    // eslint-disable-next-line prettier/prettier
+    return isValidPair(currentValue[0], currentValue[1], test.isUniqueDept, test.isUniqueLoc,)
+      ? accumultor + 1
+      : accumultor;
+  }, 0);
+
+  /*pairs.forEach((pair) => {
     if (pair.length === 2) {
       const [intern1, intern2] = pair;
 
@@ -38,12 +46,9 @@ async function uniquePairingTest(test) {
         count++;
       }
     }
-  });
+  });*/
 
-  const num_pairs = pairs.length;
-  const percent = (count / num_pairs) * 100 + "%";
-
-  console.log(test.testMessage, percent);
+  console.log(test.testMessage, accuracy(count, pairs.length));
 }
 
 uniquePairingTest({
