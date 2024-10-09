9d9cc7246623656c5662b19ef2c55e1b5e7db97b
Eli Manzo <96manzo.eli@gmail.com>
2024-10-09 15:03:13 -0700

    fix: fixing small type for filters

--- src/scripts/interns.js
+++ src/scripts/interns.js
@@ -140,7 +140,7 @@ export async function displayInternTable() {
           name: intern,
           ...internInfo,
         })),
-        filterSelections.Locations,
+        filterSelections.Location,
       ),
       filterSelections.Departments,
     ),
