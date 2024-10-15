4e1f2f02f8e7c232f2617e721213f3017dbc5d28
Jacky Perez <jackyyp2402@gmail.com>
2024-10-15 16:09:30 -0700

    feat(edit): basic remove working

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -95,3 +95,72 @@ function getSelectedInternsEdit() {
 
   return selectedInterns;
 }
+
+export function displayRemoveModal(button, pair, interns) {
+  button.onclick = function () {
+    var modal = document.getElementById("edit-pair-modal");
+
+    //display the modal
+    modal.style.display = "block";
+
+    //show interns to be added
+    removeIntern(pair, interns);
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("edit-close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+  };
+}
+
+function removeIntern(pair, interns) {
+  const modal = document.getElementById("edit-pair-modal");
+
+  const table = document.getElementById("intern-options");
+  table.innerHTML = "";
+
+  for (const intern of pair) {
+    table.appendChild(formatInternDetails(intern));
+  }
+  const idx = interns.indexOf(pair);
+  console.log(idx);
+
+  document.getElementById("submit-modal").addEventListener("click", () => {
+    const removedInterns = getSelectedInternsEdit();
+
+    //console.log(addedInterns.length);
+
+    if (removedInterns.length !== 0) {
+      //modify global selected interns
+      const names = removedInterns.map((intern) => intern.name);
+      internsSet.delete(names);
+
+      //add new interns to pair
+      pair = pair.filter((intern) => names.includes(intern.name));
+      interns[idx] = pair;
+
+      console.log(pair);
+
+      internPairsSet.clear();
+      for (const pair of interns) {
+        internPairsSet.add(pair);
+      }
+
+      //display new changes to week & intern card
+      displayInternWeekTable();
+      displayInternTable();
+    }
+
+    modal.style.display = "none";
+  });
+}
